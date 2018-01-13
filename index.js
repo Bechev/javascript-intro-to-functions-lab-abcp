8a13744ef754f94544bdc76223db3ab2f92d1c43
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-13 17:57:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-function shout(string) {
-  return string.toUpperCase();
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  string=string.toUpperCase()
+  console.log(string)
+}
+
+function logWhisper(string){
+  string=string.toLowerCase()
+  console.log(string)
+}
+
+function sayHiToGrandma(string){
+
+
+  if(string.toLowerCase() === string)
+  {
+    string = "I can't hear you!"
+  }else if(string.toUpperCase() === string)
+  {
+    string = "YES INDEED!"
+  }else if(string.toLowerCase === string)
+  {
+    string = "I can\'t hear you"
+  }else if(string === "I love you, Grandma.")
+  {
+    string ="I love you, too."
+  }
+
+return string
 }
